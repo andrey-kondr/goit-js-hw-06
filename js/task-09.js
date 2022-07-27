4855493335da85691f879bee45b7578ca9f050a6
Andrey Kondratenko <delo.ua2@gmail.com>
2022-07-27 10:15:37 +0300

    script for task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+
+const currentColor = document.querySelector(".color");
+
+const changeColorBtn = document.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  currentColor.textContent = getRandomHexColor();
+  });
