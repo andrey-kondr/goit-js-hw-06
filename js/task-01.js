60a5a2a5bd92852ce4365a4903dadac01668bbfb
Andrey Kondratenko <delo.ua2@gmail.com>
2022-07-27 11:13:25 +0300

    update script for task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,19 +1,7 @@
-const categoriesQty = document.querySelectorAll('ul#categories li.item');
-console.log(categoriesQty.length);
+const categoriesQty = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesQty.length}`);
 
-const title = document.querySelectorAll('h2');
-
-for (const categoryName of title) {
-  console.log(categoryName.textContent);
-}
-
-const categoryList = document.querySelectorAll("li.item ul");
-
-const firstCategoryLength = categoryList[0].children.length;
-console.log(firstCategoryLength);
-
-const secondCategoryLength = categoryList[1].children.length;
-console.log(secondCategoryLength);
-
-const thirdCategoryLength = categoryList[2].children.length;
-console.log(thirdCategoryLength);
+categoriesQty.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
