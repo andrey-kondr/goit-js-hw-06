6f98ef751dce0c67f9da54f80faf1aea787fdd9c
Andrey Kondratenko <delo.ua2@gmail.com>
2022-07-27 09:02:30 +0300

    script for task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
 
+const formEl = document.querySelector(".login-form");
+
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  const userDataEl = { email: email.value, Password: password.value };
+
+  console.log(userDataEl);
+  event.currentTarget.reset();
+}
