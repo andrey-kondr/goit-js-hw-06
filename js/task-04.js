f59959a18187ae0e32d332f240e43bd90e8cbcf8
Andrey Kondratenko <delo.ua2@gmail.com>
2022-08-11 21:55:19 +0300

    update task-4

--- js/task-04.js
+++ js/task-04.js
@@ -1,25 +1,20 @@
-
-
-let counterValue = document.querySelector('#value');
+let counterValue = document.querySelector("#value");
 counterValue = 0;
-
-const handleDecrementBtnClick = () => {
-    counterValue -= 1;
-    document.getElementById("value").textContent = counterValue;
-};
-
-const handleIncrementBtnClick = () => {
-    counterValue += 1;
-    document.getElementById("value").textContent = counterValue;
-};
-
-const incrementBtn = document.querySelector('button[data-action="increment"]');
-
-incrementBtn.addEventListener("click", handleIncrementBtnClick);
 
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
 
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+
+const handleDecrementBtnClick = () => {
+  counterValue -= 1;
+  document.getElementById("value").textContent = counterValue;
+};
+
+const handleIncrementBtnClick = () => {
+  counterValue += 1;
+  document.getElementById("value").textContent = counterValue;
+};
+
+incrementBtn.addEventListener("click", handleIncrementBtnClick);
+
 decrementBtn.addEventListener("click", handleDecrementBtnClick);
-
-
-
